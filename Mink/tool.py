85131febaf77f515cd31e4e7d04a166c81325ee8
seojin <dbstjwls1234@hanmail.net>
2026-04-20 16:18:26 +0200

    TasksAndLimits

--- Mink/tool.py
+++ Mink/tool.py
@@ -2,30 +2,39 @@
 import mujoco
 import numpy as np
 import mediapy as media
-from mink import Configuration, SE3, SO3, solve_ik
+from mink import Configuration, SE3, SO3, FrameTask, solve_ik
 
-def do_simulation(configuration, 
-                  task,
+def do_IK(configuration, 
+                  tasks,
                   renderer, 
                   n_frames,
                   dt,
                   target_info,
                   is_render = True,
                   solver_name = "quadprog",
-                  rendering_fps = 60):
+                  limits = [],
+                  rendering_fps = 60,
+                  is_clear_pos = True,
+                  static_pos_name = "home"):
+    if is_clear_pos:
+        configuration.update_from_keyframe(static_pos_name)
+
     frames = []
     for frame_i in range(0, n_frames):
-        # Set target
-        for target in target_info:
-            target_val = target_info[target][frame_i] # [w_o, x_o, y_o, z_o ,x,y,z]
-            new_target = SE3(wxyz_xyz = target_val)
 
-            task.set_target(new_target)
-            configuration.data.mocap_pos[0] = new_target.wxyz_xyz[4:]
-            configuration.data.mocap_quat[0] = new_target.wxyz_xyz[:4]
+        for task in tasks:
+            if type(task) is FrameTask:
+                # Set target
+                for target in target_info:
+                    target_val = target_info[target][frame_i] # [w_o, x_o, y_o, z_o ,x,y,z]
+                    new_target = SE3(wxyz_xyz = target_val)
+
+                    task.set_target(new_target)
+                    configuration.data.mocap_pos[0] = new_target.wxyz_xyz[4:]
+                    configuration.data.mocap_quat[0] = new_target.wxyz_xyz[:4]
         
         # Perform inverse kinematics
-        vel = solve_ik(configuration, [task], dt, solver=solver_name)
+        vel = solve_ik(configuration, tasks, dt, solver=solver_name, limits = limits)
         configuration.integrate_inplace(vel, dt)
         
         # Update the physical state of the simulation
