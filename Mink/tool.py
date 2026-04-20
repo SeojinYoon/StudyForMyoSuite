97315b4ed82a478ae095f2bc71180ec6ae8c62f4
seojin <dbstjwls1234@hanmail.net>
2026-04-20 15:15:06 +0200

    singularity

---
+++ Mink/tool.py
@@ -0,0 +1,42 @@
+
+import mujoco
+import numpy as np
+import mediapy as media
+from mink import Configuration, SE3, SO3, solve_ik
+
+def do_simulation(configuration, 
+                  task,
+                  renderer, 
+                  n_frames,
+                  dt,
+                  target_info,
+                  is_render = True,
+                  solver_name = "quadprog",
+                  rendering_fps = 60):
+    frames = []
+    for frame_i in range(0, n_frames):
+        # Set target
+        for target in target_info:
+            target_val = target_info[target][frame_i] # [w_o, x_o, y_o, z_o ,x,y,z]
+            new_target = SE3(wxyz_xyz = target_val)
+
+            task.set_target(new_target)
+            configuration.data.mocap_pos[0] = new_target.wxyz_xyz[4:]
+            configuration.data.mocap_quat[0] = new_target.wxyz_xyz[:4]
+        
+        # Perform inverse kinematics
+        vel = solve_ik(configuration, [task], dt, solver=solver_name)
+        configuration.integrate_inplace(vel, dt)
+        
+        # Update the physical state of the simulation
+        mujoco.mj_forward(configuration.model, configuration.data)
+        
+        # Save the current frame for video
+        renderer.update_scene(configuration.data)
+
+        if is_render:
+            pixels = renderer.render()
+            frames.append(pixels)
+
+    if is_render:
+        media.show_video(frames, fps = rendering_fps, loop = False)
